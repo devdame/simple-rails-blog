43daadfd92690b8859f0cd1f956f33c7f11187da
Lauren Scott <wisconsinpoet@gmail.com>
2014-03-09 23:24:59 -0500

    basic functionality making posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,20 @@
+class PostsController < ApplicationController
+
+	def new
+	end
+
+	def create
+	  @post = Post.create(params["post"])
+
+	  redirect_to @post
+	end
+
+	def show
+		@post = Post.find(params[:id])
+	end
+
+	def index
+		@posts = Post.all
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '6cf119788574f3afd6be65ad30e20a4ecaaaa0d7edcea852f6d767ac5fcd63b122b08f6ef06449de79144baa42bdcdbf8d219305a3d3a799c85b1d335fbe898e'
